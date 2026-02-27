b87087199fadcceb4906bbe2e28fd7551c8947b0
ComputelessComputer <john@hyprnote.com>
2026-02-27 17:53:20 +0900

    use official obsidian logo icon

--- src/components/shared/VaultPathMarquee.tsx
+++ src/components/shared/VaultPathMarquee.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState, } from "react";
+import obsidianLogo from "../../assets/2023_Obsidian_logo.svg";
 interface VaultPathMarqueeProps {
   path: string;
   className?: string;
@@ -8,18 +9,12 @@ interface VaultPathMarqueeProps {
 function PrefixIcon({ icon, }: { icon: "folder" | "obsidian"; },) {
   if (icon === "obsidian") {
     return (
-      <svg
+      <img
         className="vault-path-icon-svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+        src={obsidianLogo}
+        alt=""
         aria-hidden
-      >
-        <path d="M12 1.25L4.5 4.4L3.3 11.4L7.1 22H16.9L20.7 11.4L19.5 4.4L12 1.25Z" fill="#7C3AED" />
-        <path d="M12 4.5L8.2 9.2L10.9 19H13.1L15.8 9.2L12 4.5Z" fill="#F5F3FF" />
-        <path d="M8.2 9.2L5.9 11.7L7.7 18.3L10.9 19L8.2 9.2Z" fill="#A78BFA" />
-        <path d="M15.8 9.2L18.1 11.7L16.3 18.3L13.1 19L15.8 9.2Z" fill="#5B21B6" />
-      </svg>
+      />
     );
   }
 
